37bf37889ad9112c45d7fd4b1d7b547e478cac16
Michael Rapadas <mike@mrap.me>
2014-03-08 04:56:48 -0800

    Removes stale code

--- app/assets/javascripts/controllers.js
+++ app/assets/javascripts/controllers.js
@@ -2,28 +2,6 @@
 var controllers = angular.module('skim.controllers', []);
 
 var STARTING_WPM = 300;
-controllers.controller('SkimReaderCtrl', function($scope, $location, $http, $timeout) {
+controllers.controller('SkimReaderCtrl', function($scope) {
   $scope.wpm = STARTING_WPM;
-//   $scope.optimizedTextArray = [];
-//   $scope.currentIndex = 0;
-//   $scope.wpm = 300;
-//
-//   $scope.ms = function() {
-//     return Math.min(1000 / ($scope.wpm/60), 1000);
-//   };
-//
-//   var progressKeyword = function() {
-//     $scope.keyword = $scope.optimizedTextArray[$scope.currentIndex].word
-//     $scope.currentIndex += 1;
-//     $timeout(progressKeyword,$scope.ms());
-//     console.log($scope.ms());
-//   };
-//
-//   var optimizedDocumentURL = $location.absUrl() + '/optimized.json'
-//   $http.get(optimizedDocumentURL).success(function(data, status){
-//     $scope.optimizedTextArray = data;
-//
-//     // Timer
-//     $timeout(progressKeyword,$scope.ms());
-//   });
 });
