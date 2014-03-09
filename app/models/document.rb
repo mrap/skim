d77c9ca685d2af90b7b278e955763c6a6f3b69ef
Michael Rapadas <mike@mrap.me>
2014-03-08 17:22:50 -0800

    Documents can be sorted by most recently read

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,4 +2,7 @@ class Document
   include Mongoid::Document
 
   field :text, default: ""
+  field :last_read_at, type: DateTime
+
+  scope :recently_read, ->{ order_by(last_read_at: :desc) }
 end
--- spec/models/document_spec.rb
+++ spec/models/document_spec.rb
@@ -2,4 +2,19 @@ require 'spec_helper'
 
 describe Document do
   it { should have_field(:text).with_default_value_of("") }
+  it { should have_field(:last_read_at).of_type(DateTime) }
+
+  describe "sort by recently read" do
+    before do
+      @stale_doc = create(:document, last_read_at: DateTime.now.advance(days: -2))
+      @recent_doc = create(:document, last_read_at: DateTime.now)
+      @average_doc = create(:document, last_read_at: DateTime.now.advance(days: -1))
+    end
+
+    it "should return documents sorted by most recently read" do
+      @results = Document.recently_read
+      @results.first.should eq @recent_doc
+      @results.last.should eq @stale_doc
+    end
+  end
 end
