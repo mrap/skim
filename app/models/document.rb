4dd1f58d8cdb82a4e5b746a3f291bf60f91a6751
Michael Rapadas <mike@mrap.me>
2014-03-10 18:13:18 -0700

    Limits the maximum amount of documents.
    Keeps MongoHQ free. For now.

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,8 +2,17 @@ class Document
   include Mongoid::Document
   include Mongoid::Timestamps
 
+  MAX_DOCUMENTS_LIMIT = 100
+
   field :text, default: ""
   field :last_read_at, type: DateTime
 
   scope :recently_read, ->{ order_by(last_read_at: :desc) }
+  after_create :enforce_document_limit
+
+  # Destroys the oldest read document if over the documents limit.
+  # Required to keep MongoHQ 'free'.
+  def enforce_document_limit
+    Document.recently_read.last.destroy if Document.count > MAX_DOCUMENTS_LIMIT
+  end
 end
